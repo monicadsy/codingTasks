537f2b773ab1e87db6ef152396c2a1d278cb5290
Monica Dong <163752234+monicadsy@users.noreply.github.com>
2024-06-11 22:00:59 +0100

    Delete Task 18 directory

--- Task 18/OOP/product.py
+++
@@ -1,8 +0,0 @@
-class Product:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-
-    def __productinfo__self():
-        return f"{self.name}: £{self.price}"
-        
